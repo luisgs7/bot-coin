bd7b3da80bd6f589bcaabeaff8f84120925cd6a5
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-06 18:35:57 +0000

    refactor class

--- app/data_request/search/request_data.py
+++ app/data_request/search/request_data.py
@@ -1,6 +1,7 @@
 '''Class to perform API calls sent via the url parameter.'''
 import requests
 
+
 class RequestData:
     '''Class RequestData'''
 
@@ -9,17 +10,10 @@ class RequestData:
 
     def data(self):
         ''''Method that returns all API data'''
-        data: str = self.filter_request_bitcoin()
+        data: str = self.filter_request_data()
         print(data)
 
-    def filter_request_currencies(self):
+    def filter_request_data(self):
         '''Filter request the API'''
-        result: str = requests.get(self.url).json()['results']['currencies']
+        result: str = requests.get(self.url).json()['results']
         return result
-
-    def filter_request_bitcoin(self):
-        result: str = self.btc_mercado_bitcoin()
-        return result
-
-    def btc_mercado_bitcoin(self):
-        return requests.get(self.url).json()['results']['bitcoin']['mercadobitcoin']
