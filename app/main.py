9322d053593dce4deb4a00dd73bbdf5469f4ba5d
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-06 17:51:04 +0000

    new function main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 '''Main project file.'''
 from search import request_data
-import utils
+from utils import constants
 
+def main() -> None:
+    request_data.RequestData(url=constants.URL).data()
 
-request_data.RequestData(url=utils.constants.URL).data()
+main()
