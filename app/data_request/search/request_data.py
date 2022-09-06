2398b8adb0553d31cda3d4f0d9e78f3b8fd034a1
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-06 19:03:08 +0000

    import DATA_MOCK

--- app/data_request/search/request_data.py
+++ app/data_request/search/request_data.py
@@ -1,5 +1,6 @@
 '''Class to perform API calls sent via the url parameter.'''
 import requests
+from utils.mock import DATA_MOCK
 
 
 class RequestData:
@@ -10,10 +11,13 @@ class RequestData:
 
     def data(self):
         ''''Method that returns all API data'''
-        data: str = self.filter_request_data()
+        data: str = self.mock_data()
         print(data)
 
     def filter_request_data(self):
         '''Filter request the API'''
         result: str = requests.get(self.url).json()['results']
         return result
+
+    def mock_data(self):
+        return DATA_MOCK
