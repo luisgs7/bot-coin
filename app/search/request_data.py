2e881868bd8867e4be8c68e9f907cbe9c17e10d8
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-06 14:47:23 +0000

    add methods filter request

--- app/search/request_data.py
+++ app/search/request_data.py
@@ -9,5 +9,14 @@ class RequestData:
 
     def data(self):
         ''''Method that returns all API data'''
-        data = requests.get(self.url).json()
+        data: str = self.filter_request_currencies()
         print(data)
+
+    def filter_request_currencies(self):
+        '''Filter request the API'''
+        result: str = requests.get(self.url).json()['results']['currencies']
+        return result
+
+    def filter_request_bitcoin(self):
+        result: str = requests.get(self.url).json()['results']['bitcoin']
+        return result
