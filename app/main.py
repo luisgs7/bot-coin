ce0a18616909b475eb617e1f9a56b3f51235f5cd
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-06 20:21:40 +0000

    refactor main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 '''Main project file.'''
 from data_request.search import request_data
 from utils import constants
+from data_cleaning.cleaning import cleaning
 
 
 def main() -> None:
     '''Function Main'''
-    request_data.RequestData(url=constants.URL).data()
+    result = request_data.RequestData(url=constants.URL).data()
+    data = cleaning(result).clear_cureencies()
+    print(data)
 
 main()
