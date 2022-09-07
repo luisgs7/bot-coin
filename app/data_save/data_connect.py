82dbd9900aee790b130050a2939b575db07281e7
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-07 23:51:52 +0000

    new files for data_save

--- app/data_save/data_connect.py
+++ app/data_save/data_connect.py
@@ -1,61 +1,46 @@
-import sqlite3
-import os
+import psycopg2
+from sql import func_query
+from decouple import config
+
+_BASE_URL: str = config("DATABASE_URL")
 
 
 class DataConnect():
 
     def __init__(self):
-        self.table_name = "table_coin"
-        self.__dir_db = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        self.con = sqlite3.connect(f"{self.__dir_db}/app.db")
-        self.cur = self.con.cursor()
-    
-    def select_curencies(self):
-        res = self.cur.execute(
-            f"SELECT id, name, buy, sell, variation \
-            FROM {self.table_name} \
-            WHERE id and 1" \
-        )
-        res = res.fetchall()
+        self._con = psycopg2.connect(_BASE_URL)
+        self.cur = self._con.cursor()
+        self._tb_name = "table_coin"
+        print("Database runing...")
+
+    def select_coin(self):
+        res = self.cur.execute(func_query.query_select_value(self._tb_name))
+        res = self.cur.fetchone()
         return res
     
-    def insert_coin(self, id: int, name: str, buy: float, sell: float, variation: float):
-        self.cur.execute(
-            f"INSERT INTO {self.table_name} VALUES({id}, '{name}', {buy}, {sell}, {variation})"
-        )
-        self.con.commit()
+    def _insert_coin(self, name: str, buy: float, sell: float, variation: float):
+        self.cur.execute(func_query.query_insert_value(self._tb_name, name, buy, sell, variation))
+        self._con.commit()
         return None
     
     def update_coin(self, id: int, name: str, buy: float, sell: float, variation: float):
-        self.cur.execute(
-            f"UPDATE {self.table_name} SET \
-              name='{name}', \
-              buy={buy}, \
-              sell={sell}, \
-              variation={variation} \
-              WHERE id=1" \
-        )
-        self.con.commit()
+        self.cur.execute(func_query.query_update_value(self._tb_name, id, name, buy, sell, variation))
+        self._con.commit()
         return None
 
-    def create_table(self):
-        res = self.cur.execute("CREATE TABLE IF NOT EXISTS table_coin( \
-                                id INT NOT NULL, \
-                                name varchar, \
-                                buy NUMERIC, \
-                                sell NUMERIC, \
-                                variation NUMERIC)")
-        
-        return None
-    
-    def drop_table(self):
-        self.cur.execute("DROP TABLE table_coin")
-        return "OK"
+    def _create_table(self):
+        res = self.cur.execute(func_query.query_create_table(self._tb_name))
+        return f"Criada a {self._tb_name}"
+
+
 pessoa = "Pedro"
 pessoa2 = "José"
 #print(f"Create Table: {DataConnect().create_table()}")
-#print(f"Insert values: {DataConnect().insert_coin(id=1, name=pessoa, buy=1.34, sell=1.56, variation=-1.34)}")
-print(f"SELECT Values: {DataConnect().select_curencies()}")
-#print(f"Update Values: {DataConnect().update_coin(id=1, name=pessoa2, buy=2.4, sell=3.8, variation=1.43)}")
+#print(f"Insert values: {DataConnect().insert_coin(name=pessoa, buy=1.34, sell=1.56, variation=-1.34)}")
 
-#print(f'Drop Table: {DataConnect().drop_table()}')
+#print(a[0])
+#print(f"Update Values: {DataConnect().update_coin(id=3,name=pessoa2, buy=2.4, sell=3.8, variation=1.43)}")
+
+a = DataConnect().select_coin()
+print(f"SELECT Values: {a}")
+
