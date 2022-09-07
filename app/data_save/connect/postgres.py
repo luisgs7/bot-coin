ead1176336a3f0e2064323781d9d717a38576c0d
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-07 20:25:26 +0000

    update config

--- app/data_save/connect/postgres.py
+++ app/data_save/connect/postgres.py
@@ -1,21 +1,25 @@
 import psycopg2
 from sql import func_query
+from decouple import config
+
+BASE_URL: str = config("DATABASE_URL")
 
 
 class DataConnect():
 
     def __init__(self):
-        self.con = psycopg2.connect("postgres://nxmhpaof:buSco67i267kihD7ouvLjE7grssbOKJk@kesavan.db.elephantsql.com/nxmhpaof")
+        self.con = psycopg2.connect(BASE_URL)
         self.cur = self.con.cursor()
         self._tb_name = "table_coin"
+        print("Conectado...")
 
     def select_curencies(self):
         res = self.cur.execute(func_query.query_select_value(self._tb_name))
-        res = res.fetchall()
+        res = self.cur.fetchall()
         return res
     
-    def insert_coin(self, id: int, name: str, buy: float, sell: float, variation: float):
-        self.cur.execute(func_query.query_insert_value(self._tb_name, id, name, buy, sell, variation))
+    def insert_coin(self, name: str, buy: float, sell: float, variation: float):
+        self.cur.execute(func_query.query_insert_value(self._tb_name, name, buy, sell, variation))
         self.con.commit()
         return None
     
@@ -26,7 +30,7 @@ class DataConnect():
 
     def create_table(self):
         res = self.cur.execute(func_query.query_create_table(self._tb_name))
-        return None
+        return f"Criada a {self._tb_name}"
     
     def drop_table(self):
         self.cur.execute(func_query.query_drop_db(self._tb_name))
@@ -36,12 +40,13 @@ class DataConnect():
 
 pessoa = "Pedro"
 pessoa2 = "José"
-print(f"Create Table: {DataConnect().create_table()}")
-#print(f"Insert values: {DataConnect().insert_coin(id=1, name=pessoa, buy=1.34, sell=1.56, variation=-1.34)}")
-#a = DataConnect().select_curencies()
-#print(f"SELECT Values: {a}")
-#print(a[0])
-#print(f"Update Values: {DataConnect().update_coin(id=1, name=pessoa2, buy=2.4, sell=3.8, variation=1.43)}")
+#print(f"Create Table: {DataConnect().create_table()}")
+#print(f"Insert values: {DataConnect().insert_coin(name=pessoa, buy=1.34, sell=1.56, variation=-1.34)}")
 
-#print(f'Drop Table: {DataConnect().drop_table()}')
-#print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+#print(a[0])
+#print(f"Update Values: {DataConnect().update_coin(id=3,name=pessoa2, buy=2.4, sell=3.8, variation=1.43)}")
+
+a = DataConnect().select_curencies()
+print(f"SELECT Values: {a}")
+
+print(f'Drop Table: {DataConnect().drop_table()}')
