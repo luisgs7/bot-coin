3e833125b086e680305c35f75cc1e6de525fb45d
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-07 17:32:05 +0000

    new querys SQL

--- app/data_save/connect/sql/query.sql
+++ app/data_save/connect/sql/query.sql
@@ -1,11 +1,21 @@
-CREATE TABLE = "
-        CREATE TABLE table_coin (
-            id SERIAL PRIMAY KEY, 
-            name VARCHAR(15),
-            buy , 
-            sell, 
-            variation );
-"
-g1 tocantins
+""" Create Table """
+CREATE TABLE table_coin (id SERIAL PRIMAY KEY, 
+                        name VARCHAR(15),
+                        buy smallint, 
+                        sell smallint, 
+                        variation smallint);
+                    
+
+"""Insert Value"""
+INSERT INTO table_coin VALUES (id, 
+                               'name',
+                               buy, 
+                               sell, 
+                               variation);
+
+"""DROP TABLE"""
+DROP TABLE table_coin;
+
+
 
 
