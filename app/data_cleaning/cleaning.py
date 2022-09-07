f48b4fe9c4d6fc996f839822c66429acd5d3f95f
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-07 00:09:13 +0000

    new method clear_btc

--- app/data_cleaning/cleaning.py
+++ app/data_cleaning/cleaning.py
@@ -1,13 +1,14 @@
 '''Clean up the data provided by the API'''
+from typing import Dict
 
 class cleaning():
     def __init__(self, data: dict):
         self.data = data
         self.keys = ['USD', 'EUR', 'CAD']
-        self.btc = ['']
+        self.btc = ['mercadobitcoin']
 
     def clear_cureencies(self):
-        result = []
+        result: Dict = []
         for key in self.keys:
             result.append(self.data['currencies'][key]['name'])
             result.append(self.data['currencies'][key]['buy'])
@@ -15,6 +16,13 @@ class cleaning():
             result.append(self.data['currencies'][key]['variation'])
 
         return result
-    
+
     def clear_btc(self):
-        pass
+        result: Dict = []
+        for btc in self.btc:
+            result.append(self.data['bitcoin'][btc]['name'])
+            result.append(self.data['bitcoin'][btc]['buy'])
+            result.append(self.data['bitcoin'][btc]['sell'])
+            result.append(self.data['bitcoin'][btc]['variation'])
+        
+        return result
