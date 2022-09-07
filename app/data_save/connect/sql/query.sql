4b1b9cc9734ddf8a0dc820ec6f50f13e502c7daf
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-07 15:01:44 +0000

    sql file

---
+++ app/data_save/connect/sql/query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE = "
+        CREATE TABLE table_coin (
+            id SERIAL PRIMAY KEY, 
+            name VARCHAR(15),
+            buy , 
+            sell, 
+            variation );
+"
+g1 tocantins
+
+
