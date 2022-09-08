38181ee00958820b06570071289bcb23d1a84cf9
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-08 01:06:07 +0000

    refactor module

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 '''Main project file.'''
 from data_request.search import request_data
 from utils import constants
-from data_cleaning.cleaning import cleaning
+from data_cleaning.cleaning import cleaning_data
 
 
 def main() -> None:
     '''Function Main'''
+    print("App running...")
     result = request_data.RequestData(url=constants.URL).data()
-    data = cleaning(result).clear_curencies()
-    btc = cleaning(result).clear_btc()
-    print(f"Data: {data}")
-    print(f"BTC: {btc}")
+    data = cleaning_data(result).clear_curencies()
+    btc = cleaning_data(result).clear_btc()
+    print("Updated data.")
 
 main()
