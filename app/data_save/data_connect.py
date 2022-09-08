84fc7b12452e1c0ed23ed1b7d0331d6809187b6c
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-08 01:07:40 +0000

    refactor messages

--- app/data_save/data_connect.py
+++ app/data_save/data_connect.py
@@ -1,5 +1,5 @@
 import psycopg2
-from sql import func_query
+from data_save.sql import func_query
 from decouple import config
 
 _BASE_URL: str = config("DATABASE_URL")
@@ -11,7 +11,7 @@ class DataConnect():
         self._con = psycopg2.connect(_BASE_URL)
         self.cur = self._con.cursor()
         self._tb_name = "table_coin"
-        print("Database runing...")
+        print("Database running...")
 
     def select_coin(self):
         res = self.cur.execute(func_query.query_select_value(self._tb_name))
@@ -26,8 +26,9 @@ class DataConnect():
     def update_coin(self, id: int, name: str, buy: float, sell: float, variation: float):
         self.cur.execute(func_query.query_update_value(self._tb_name, id, name, buy, sell, variation))
         self._con.commit()
+        print("Update data...")
         return None
 
     def _create_table(self):
         res = self.cur.execute(func_query.query_create_table(self._tb_name))
-        return f"Criada a {self._tb_name}"
+        return None
