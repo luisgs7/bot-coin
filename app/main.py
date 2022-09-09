56e7704976457c38758becb821dcd46d19e074d1
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-09 20:03:16 +0000

    add scheduler

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 '''Main project file.'''
+import sched
 from data_request.search import request_data
 from utils import constants
 from data_cleaning.cleaning import cleaning_data
 
+scheduler = sched.scheduler()
 
 def main() -> None:
     '''Function Main'''
     print("App running...")
     result = request_data.RequestData(url=constants.URL).data()
-    data = cleaning_data(result).clear_curencies()
-    btc = cleaning_data(result).clear_btc()
-    print("Updated data.")
+    cleaning_data(result).clear_curencies()
+    cleaning_data(result).clear_btc()
+    print("END.")
+    scheduler.enter(delay=(constants.TIME), priority=0, action=main)
+    print(f"Waiting: {constants.TIME} seconds...")
 
 main()
+
+scheduler.run(blocking=True)
