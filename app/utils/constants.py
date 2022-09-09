be56edf5d92cf73284c299fbd35cc6e6b1a63f43
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-09 19:37:37 +0000

    add new constants

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,9 +1,14 @@
 '''This file contains constants used in the project.'''
 from decouple import config
+from typing import List
 
 
-_SECRET_KEY: str = config('SECRET_KEY')
+SECRET_KEY: str = config('SECRET_KEY')
 
-URL = f'https://api.hgbrasil.com/finance?key={_SECRET_KEY}'
+COINS: List[str] = ['USD', 'EUR', 'CAD', 'JPY']
+BTC: List[str] = ['mercadobitcoin']
+LENGHT: int = len(COINS + BTC)
 
-# URL = f'https://api.hgbrasil.com/finance?format=json&key={_SECRET_KEY}'
+URL = f'https://api.hgbrasil.com/finance?key={SECRET_KEY}'
+
+# URL = f'https://api.hgbrasil.com/finance?format=json&key={SECRET_KEY}'
