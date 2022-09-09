9dff4a101fb9de0c42a4007bf3551ac99f580b80
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-09 20:03:39 +0000

    add time key

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -4,11 +4,13 @@ from typing import List
 
 
 SECRET_KEY: str = config('SECRET_KEY')
+URL = f'https://api.hgbrasil.com/finance?key={SECRET_KEY}'
+TIME: int = int(config('TIME'))
+
 
 COINS: List[str] = ['USD', 'EUR', 'CAD', 'JPY']
 BTC: List[str] = ['mercadobitcoin']
 LENGHT: int = len(COINS + BTC)
 
-URL = f'https://api.hgbrasil.com/finance?key={SECRET_KEY}'
 
 # URL = f'https://api.hgbrasil.com/finance?format=json&key={SECRET_KEY}'
