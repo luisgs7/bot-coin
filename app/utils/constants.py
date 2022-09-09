4d3b8c1241c24e2ac55e5899bd95e82a86351088
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-09 17:38:42 +0000

    rename module constantes

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -2,6 +2,8 @@
 from decouple import config
 
 
-__SECRET_KEY: str = config('SECRET_KEY')
+_SECRET_KEY: str = config('SECRET_KEY')
 
-URL = f'https://api.hgbrasil.com/finance?key={__SECRET_KEY}'
+URL = f'https://api.hgbrasil.com/finance?key={_SECRET_KEY}'
+
+# URL = f'https://api.hgbrasil.com/finance?format=json&key={_SECRET_KEY}'
