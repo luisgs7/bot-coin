ee2e12531379f9e468d4e916c13aeb3757d73ad1
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-12 15:02:21 +0000

    add TABLE_NAME and BASE_URL

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -7,10 +7,10 @@ SECRET_KEY: str = config("SECRET_KEY")
 URL = f"https://api.hgbrasil.com/finance?key={SECRET_KEY}"
 TIME: int = int(config("TIME"))
 
+TABLE_NAME: str = config("TABLE_NAME")
+BASE_URL: str = config("DATABASE_URL")
+
 
 COINS: List[str] = ["USD", "EUR", "CAD", "JPY"]
 BTC: List[str] = ["mercadobitcoin"]
 LENGHT: int = len(COINS + BTC)
-
-
-# URL = f'https://api.hgbrasil.com/finance?format=json&key={SECRET_KEY}'
