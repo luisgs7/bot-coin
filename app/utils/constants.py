adac48a2a4d72bb33a99b58e09b73c0ef056587b
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-13 00:33:46 +0000

    update database config

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -3,17 +3,13 @@ from typing import List
 from decouple import config
 
 
-DB_USER = config("DB_USER")
-DB_PASS = config("DB_PASS")
-DB_NAME = config("DB_NAME")
-DB_HOST = config("DB_HOST")
-
-SECRET_KEY: str = "f2fcc2cc"
+SECRET_KEY: str = config("SECRET_KEY")
 URL = f"https://api.hgbrasil.com/finance?key={SECRET_KEY}"
 TIME: int = int(config("TIME"))
 
+
 TABLE_NAME: str = config("TABLE_NAME")
-BASE_URL: str = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+BASE_URL: str = config("DATABASE_URL")
 
 
 COINS: List[str] = ["USD", "EUR", "CAD", "JPY"]
