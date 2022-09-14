1705ecb92f1168c3d67f1c90904da3458bf47dff
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-14 17:44:29 +0000

    rename folder

--- bot/start.py
+++ bot/start.py
@@ -1,1 +1,16 @@
-print("Bot Ola :)")
+"""Main project file."""
+import sched
+scheduler = sched.scheduler()
+
+
+def hello() -> None:
+    """Function Main"""
+    print("BOT BOT BOT...")
+    scheduler.enter(delay=(2), priority=0, action=hello)
+    print("BOT Waiting: 2 seconds...")
+
+
+hello()
+
+scheduler.run(blocking=True)
+
