0ef66e0f94da60d2b3b3ec0e04469f290d56790c
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-16 00:06:07 +0000

    refactor function main

--- data/main.py
+++ data/main.py
@@ -1,28 +1,17 @@
 """Main project file."""
 import sched
-from datetime import datetime, timezone, timedelta
-from data_request import request_data
+from data_request import request
+from data_save import save
 from utils import constants
-from data_cleaning.cleaning import CleaningData
 
 scheduler = sched.scheduler()
-
-difference = timedelta(hours=-3)
-time_zone = timezone(difference)
-date_now = datetime.now()
-date_and_time_sp = date_now.astimezone(time_zone)
-date_now_text = date_and_time_sp.strftime("%d/%m/%Y %H:%M")
 
 
 def main() -> None:
     """Function Main"""
     print("App running...")
-    result = request_data.RequestData(url=constants.URL).data()
-    if result is None:
-        print(f"Problems with the API: {date_now_text}")
-    else:
-        CleaningData(result).clear_currencies()
-        CleaningData(result).clear_btc()
+    result = request.present_data_request()
+    save.present_data_save(result)
     print("END.")
     scheduler.enter(delay=(constants.TIME), priority=0, action=main)
     print(f"Waiting: {constants.TIME} seconds...")
