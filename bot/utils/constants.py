e24978c0e9787a7f0a764e50a7104d266cb13171
luisgs7 <luisgsilva270@gmail.com>
2022-09-18 15:56:58 -0300

    new constants

--- bot/utils/constants.py
+++ bot/utils/constants.py
@@ -3,3 +3,7 @@ from decouple import config
 
 DATABASE_URL = config("DATABASE_URL")
 TABLE_NAME = config("TABLE_NAME")
+
+TOKEN: str = config("TELEGRAM_BOT_TOKEN")
+TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/'
+MESSAGE_ABOUT = config("MESSAGE_ABOUT")
