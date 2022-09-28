052aa6e0297aa521ef09130d63752460321a2918
luisgs7 <luisgsilva270@gmail.com>
2022-09-28 15:16:06 -0300

    add print log

--- bot/query_response/func_query/query.py
+++ bot/query_response/func_query/query.py
@@ -3,6 +3,7 @@
 
 def query_select_value(table_name: str, _id) -> str:
     '''Function that performs a select on the corresponding table.'''
+    print("Bot querying the data...")
     query = f"SELECT name, buy, variation, date_time \
             FROM {table_name} where id = {_id};"
     return query
