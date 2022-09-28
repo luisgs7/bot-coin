9d8efa4659badc24910820ea0551cd1121490a93
Luís Gustavo <luisgsilva270@gmail.com>
2022-09-28 01:35:23 +0000

    add script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Let's Go!"
+
+read  -p "Digite o nome do usuário do banco de dados: " database_user
+
+sudo docker-compose down
+
+sudo docker-compose up -d
+echo "Wait 5 seconds, the database is initializing..."
+sleep 5
+
+sudo docker-compose exec db psql -U $database_user -d bot_data -f  /scripts/insert.sql
+
+sudo docker compose down
+
+sudo docker-compose up
+echo "Finished."
